f8b1a960c9f5c35821fffdc98c21421475778ccb
kanayofirms <kanyeze@gmail.com>
2023-06-18 11:37:01 +0100

    Import customer collection and paginate for scalability

--- laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
+++ laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
+use App\Http\Resources\V1\CustomerCollection;
 
 class CustomerController extends Controller
 {
@@ -17,7 +18,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return Customer::all();
+        return new CustomerCollection(Customer::paginate());
     }
 
     /**
@@ -49,7 +50,7 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        return $customer;
+        return new CustomerResource($customer);
     }
 
     /**
