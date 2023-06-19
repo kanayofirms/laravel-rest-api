aaa0f743ef1bea4275ab046838c2210e81be27bb
kanayofirms <kanyeze@gmail.com>
2023-06-19 22:14:52 +0100

    Create new objects in index function and show function for invoice endpoint

--- laravel-rest-api/app/Http/Controllers/Api/V1/InvoiceController.php
+++ laravel-rest-api/app/Http/Controllers/Api/V1/InvoiceController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
-
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
 
 class InvoiceController extends Controller
 {
@@ -16,7 +18,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        //
+        return new InvoiceCollection(Invoice::paginate());
     }
 
     /**
@@ -48,7 +50,7 @@ class InvoiceController extends Controller
      */
     public function show(Invoice $invoice)
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
