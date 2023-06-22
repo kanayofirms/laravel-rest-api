82b1972b72e87a2bc8ff678fc1e3887ce87d6924
kanayofirms <kanyeze@gmail.com>
2023-06-22 02:42:14 +0100

    Rename file for readability

--- laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
+++ laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
@@ -9,7 +9,7 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
-use App\Services\V1\CustomerQuery;
+use App\Filters\V1\CustomersFilter;
 
 class CustomerController extends Controller
 {
@@ -20,7 +20,7 @@ class CustomerController extends Controller
      */
     public function index(Request $request)
     {
-        $filter = new CustomerQuery();
+        $filter = new CustomersFilter();
         $queryItems = $filter->transform($request); // [['column', 'operator', 'value']]
 
         if (count($queryItems) ==0) {
