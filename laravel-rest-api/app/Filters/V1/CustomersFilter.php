a2eaaf8430dc7ae70b1e95636155d07f439403a1
kanayofirms <kanyeze@gmail.com>
2023-06-24 02:49:07 +0100

    Delete the transform method for reusability

--- laravel-rest-api/app/Filters/V1/CustomersFilter.php
+++ laravel-rest-api/app/Filters/V1/CustomersFilter.php
@@ -26,28 +26,5 @@ class CustomersFilter extends ApiFilter {
         'lte' => '<=',
         'gt' => '>',
         'gte' => '>=',
-    ];
-
-    public function transform(Request $request) {
-        $eloquery = [];
-
-        foreach ($this->safeParams as $param => $operators) {
-            $query = $request->query($param);
-
-            if (!isset($query)) {
-                continue;
-            }
-
-            $column = $this->columnMap[$param] ?? $param;
-
-            foreach ($operators as $operator) {
-                if (isset($query[$operator])) {
-                    $eloquery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
-                }
-            }
-        }
-
-        return $eloquery;
-    }
-   
+    ];   
 }
