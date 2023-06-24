0e75ea659ed9a705d47fbe0fd100fab37c8e9555
kanayofirms <kanyeze@gmail.com>
2023-06-24 04:04:00 +0100

    Import Http Request

--- laravel-rest-api/app/Http/Controllers/Api/V1/InvoiceController.php
+++ laravel-rest-api/app/Http/Controllers/Api/V1/InvoiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Invoice;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Controllers\Controller;
