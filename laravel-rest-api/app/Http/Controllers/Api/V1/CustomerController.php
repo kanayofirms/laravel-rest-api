f47aaafcb75be44c1e295d850af13aeba8cf58bd
kanayofirms <kanyeze@gmail.com>
2023-07-01 21:59:10 +0100

    Import client requests for version one

--- laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
+++ laravel-rest-api/app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
