7c30bf5d04c685a135f644207a8c3950a5d2d5d1
kanayofirms <kanyeze@gmail.com>
2023-07-02 02:50:58 +0100

    Create setup for authentication

--- laravel-rest-api/routes/web.php
+++ laravel-rest-api/routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/setup', function() {
+    $credentials = [
+        'email' => 'admin@admin.com', 
+        'password' => 'password'
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        $user = new \App\Models\User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+    }
+})
